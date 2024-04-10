a0c029caad934606f4abf2f16c0b4ddd96f5e697
Lamers <gustav@henrikssons.org>
2024-04-10 21:46:25 +0200

    trying to get the client auth with visma to work

--- src/main/java/begh/vismaauthservice/Controller.java
+++ src/main/java/begh/vismaauthservice/Controller.java
@@ -1,31 +1,32 @@
 package begh.vismaauthservice;
 
-import com.nimbusds.oauth2.sdk.token.Tokens;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    private final VismaAuthService vismaAuthService;
-
-    public Controller(VismaAuthService vismaAuthService) {
-        this.vismaAuthService = vismaAuthService;
-    }
-
-    @GetMapping("/refresh-token")
-    public ResponseEntity<Tokens> refreshToken() {
-        Tokens tokens = vismaAuthService.refreshTokens();
-        if (tokens != null) {
-            return ResponseEntity.ok(tokens);
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @GetMapping("/token")
-    public ResponseEntity<Tokens> getToken() {
-        return ResponseEntity.ok(vismaAuthService.getTokens());
+//    private final VismaAuthService vismaAuthService;
+//
+//    public Controller(VismaAuthService vismaAuthService) {
+//        this.vismaAuthService = vismaAuthService;
+//    }
+//
+//    @GetMapping("/refresh-token")
+//    public ResponseEntity<Tokens> refreshToken() {
+//        Tokens tokens = vismaAuthService.refreshTokens();
+//        if (tokens != null) {
+//            return ResponseEntity.ok(tokens);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @GetMapping("/token")
+//    public ResponseEntity<Tokens> getToken() {
+//        return ResponseEntity.ok(vismaAuthService.getTokens());
+//    }
+    @GetMapping()
+    public String sayHello(){
+        return "Hello";
     }
 }
