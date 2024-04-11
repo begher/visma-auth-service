60ef9f12a378aa1b6ff1efb09955957f438f8c37
Lamers <gustav@henrikssons.org>
2024-04-11 13:31:21 +0200

    changed grant type

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
+	//compileOnly("org.projectlombok:lombok")
+	//runtimeOnly("org.postgresql:postgresql")
+	//annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
--- src/main/java/begh/vismaauthservice/access/AccessRepository.java
+++ src/main/java/begh/vismaauthservice/access/AccessRepository.java
@@ -1,8 +1,8 @@
-package begh.vismaauthservice.access;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccessRepository extends JpaRepository<Access, Integer> {
-}
+//package begh.vismaauthservice.access;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AccessRepository extends JpaRepository<Access, Integer> {
+//}
